a82771a57a3989f7647ba8910c94320560bad91b
Anthony Capobianco <anth@live.be>
2017-10-24 20:33:43 +0200

    ex 11

---
+++ loopsExerciseEleven/loopsExerciseEleven/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  loopsExerciseEleven
+//
+//  Created by Anthony Capobianco on 24/10/2017.
+//  Copyright © 2017 Anthony Capobianco. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int n=0
+        ,a;
+    //while the number is greater or equal to 0 do
+    do{
+        printf("Veuillez insérer un nombre entier positif ou nul:\n");
+        scanf("%i", &a );
+        //if the number is equal to or greater than 0 than set n as n+a else n
+        a>=0?n+=a:n;
+    }while (a >= 0);
+    //print the sum of all the integers that have been entered prior to a negative number discarding the latter
+    printf("La somme des nombres entiers positif est de: %i\n", n);
+    
+    return 0;
+}
